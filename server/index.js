c15c65448cc957b2b9c71f9533305bf66cad9d53
Gaurav Thakur <gaurav1234thakurgt@gmail.com>
2024-03-15 18:39:55 +0530

    Renamed server.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,10 @@ const routes = require('./routes/routes.js');
 const cors=require('cors');
 require('dotenv').config(); 
 
-const app = express();
+const app = express(); 
 app.use(cors());
-const PORT =  5000;
-
-mongoose.connect("mongodb+srv://gaurav1234thakurgt:1234@easyride.hv31can.mongodb.net/CabBooking?retryWrites=true&w=majority&appName=EasyRide/CabBooking", { useNewUrlParser: true, useUnifiedTopology: true })
+const port =  process.env.PORT;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB Atlas');
     })
@@ -16,9 +15,10 @@ mongoose.connect("mongodb+srv://gaurav1234thakurgt:1234@easyride.hv31can.mongodb
 
 app.use('/', routes);
 app.use('/location', routes);
-app.use('/cabs', routes);
-app.use('/cabs/update/:id', routes);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use('/cabs', routes); 
+app.use('/cabs/update/:id', routes);  
+ 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
+ 
